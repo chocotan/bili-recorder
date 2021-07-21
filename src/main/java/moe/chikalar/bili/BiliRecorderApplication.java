6e1b6963a09151e2b890932b362455b551630a95
chocotan <loli@linux.com>
2021-07-21 09:30:42 +0800

    删除无用类

--- src/main/java/moe/chikalar/bili/BiliRecorderApplication.java
+++ src/main/java/moe/chikalar/bili/BiliRecorderApplication.java
@@ -1,12 +1,9 @@
 package moe.chikalar.bili;
 
-import moe.chikalar.bili.configuration.BiliRecorderProperties;
 import moe.chikalar.bili.utils.HttpsTrustManager;
-import org.apache.catalina.servlets.DefaultServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -26,17 +23,6 @@ public class BiliRecorderApplication {
     @Bean
     public LinkedList<Long> recordQueue() {
         return new LinkedList<>();
-    }
-    @Bean
-    public ServletRegistrationBean servletRegistrationBean(BiliRecorderProperties properties) {
-        final DefaultFileServlet servlet = new DefaultFileServlet();
-        final ServletRegistrationBean bean =
-                new ServletRegistrationBean(servlet, "/data/*");
-        bean.addInitParameter("listings", "true");
-        bean.addInitParameter("sortListings", "true");
-        bean.addInitParameter("folder", properties.getWorkPath());
-        bean.setLoadOnStartup(1);
-        return bean;
     }
 
 }
